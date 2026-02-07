f05208a250781ac078be0a893b2a8de484007d24
kashishsharma-learns <kashish.sharmaa6123@gmail.com>
2026-02-07 12:37:48 +0530

    Add files via upload

--- K means clustering/k means.py
+++ K means clustering/k means.py
@@ -17,4 +17,5 @@ plt.scatter(x[:,0],x[:,1],c=labels,cmap='viridis',s=30,alpha=0.6)
 plt.scatter(centers[:,0],centers[:,1],c='red',s=200,marker='X',label='Centroids')
 plt.title("K-means Clustering(k=3)")
 plt.legend()
+
 plt.show()
